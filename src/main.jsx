412447761c80b1f490f7a34ba603920a04e30598
r11921a37 <r11921a37@ntu.edu.tw>
2023-04-08 20:13:32 +0800

    hash router

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import About from "./pages/about";
 import "./index.css";
 const router = createHashRouter([
   {
-    path: "/",
+    path: "/*",
     element: <RootLayout />,
     children: [
       { index: true, element: <RootIndex /> },
@@ -24,14 +24,14 @@ const router = createHashRouter([
   },
 ]);
 
-// ReactDOM.createRoot(document.getElementById("root")).render
-//   (
-//     <React.StrictMode>
-//       <RouterProvider router={router} />
-//     </React.StrictMode>
-//   );
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render
+  (
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  );
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
