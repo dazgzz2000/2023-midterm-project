bcb3c1a060332f5f2e5b3667249168bb28b6f99e
r11921a37 <r11921a37@ntu.edu.tw>
2023-04-08 18:41:06 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Welcome from './Welcome'
+import ColorText from './ColorText'
+function App() {
+  return (
+    <div>
+      <Welcome name="Yiting" />
+      <ColorText color="yellow" message="OwO" />
+    </div>
+  );
+}
+export default App;
---
+++ src/pages/about.jsx
@@ -0,0 +1,3 @@
+export default function About() {
+    return <dev><h1>About</h1> <h1 className="text-3xl font-bold text-left underline text-orange-600"> Hello World!</h1></dev>;
+}
