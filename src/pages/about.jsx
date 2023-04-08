39a3ed6e34eb8f6e765360e46e2cd0d48d2d75ca
r11921a37 <r11921a37@ntu.edu.tw>
2023-04-08 20:38:29 +0800

    about

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,3 +1,8 @@
 export default function About() {
-    return <dev><h1>About</h1> <h1 className="text-3xl font-bold text-left underline text-orange-600"> Hello World!</h1></dev>;
+    return <dev>
+        <h1>About</h1> 
+        <h1 className="text-3xl font-bold text-left underline text-orange-600"> Hello World!</h1>
+        <h1>r11921a37's website</h1>
+        <h2>ABOUT ME</h2>
+        </dev>;
 }
